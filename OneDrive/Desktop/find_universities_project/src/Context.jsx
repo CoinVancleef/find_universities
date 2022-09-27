d14eb76e0736a635bf47707e233760a22235fee7
Arsenii Tsubera <111arsen111@gmail.com>
2022-09-27 16:30:01 +0300

    hot fix

--- OneDrive/Desktop/find_universities_project/src/Context.jsx
+++ OneDrive/Desktop/find_universities_project/src/Context.jsx
@@ -4,6 +4,7 @@ const Context = React.createContext();
 
 function ContextProvider({ children }) {
   const [countriesData, setCountriesData] = useState([]);
+  const [updatedCountriesData, setUpdatedCountriesData] = useState([]);
   const countriesURL = "https://restcountries.com/v2/all?fields=name,flags";
   useEffect(() => {
     fetch(countriesURL)
@@ -11,8 +12,21 @@ function ContextProvider({ children }) {
       .then((data) => setCountriesData(data));
   }, []);
 
+  const updated = countriesData.map((country) => {
+    return country.name === "United States of America"
+      ? { ...country, name: "United States" }
+      : country;
+  });
+
+  useEffect(() => {
+    setUpdatedCountriesData(updated);
+  }, [countriesData]);
+
+  console.log(updatedCountriesData);
   return (
-    <Context.Provider value={{ countriesData }}>{children}</Context.Provider>
+    <Context.Provider value={{ updatedCountriesData }}>
+      {children}
+    </Context.Provider>
   );
 }
 
--- OneDrive/Desktop/find_universities_project/src/pages/Main.jsx
+++ OneDrive/Desktop/find_universities_project/src/pages/Main.jsx
@@ -4,8 +4,8 @@ import { Context } from "../Context";
 import { Link } from "react-router-dom";
 
 export default function Main() {
-  const { countriesData } = useContext(Context);
-  const allCountries = countriesData.map((country) => (
+  const { updatedCountriesData } = useContext(Context);
+  const allCountries = updatedCountriesData.map((country) => (
     <Link to={`/countries/${country.name}`}>
       <Country countryName={country.name} img={country.flags.svg} />
     </Link>
