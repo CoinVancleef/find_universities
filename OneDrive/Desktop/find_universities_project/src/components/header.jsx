dc7b7f0603277b8346372d671cdc89098a13e3bf
Arsenii Tsubera <111arsen111@gmail.com>
2022-09-30 21:08:20 +0300

    added red circle when added favorites

--- OneDrive/Desktop/find_universities_project/src/components/header.jsx
+++ OneDrive/Desktop/find_universities_project/src/components/header.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context";
+import redCircle from "../assets/red_circle.png";
 
 export default function Header() {
+  const { favoriteUniArray } = useContext(Context);
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h3>Find your University!</h3>
         </Link>
-        <Link to="/favorited">
+        <Link className="fav" to="/favorited">
           <h3>Favorites</h3>
+          {favoriteUniArray.length > 0 && (
+            <p className="dot">{favoriteUniArray.length}</p>
+          )}
         </Link>
       </div>
     </header>
